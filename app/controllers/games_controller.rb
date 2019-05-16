2c0d41498f7b1bc2a8944a374911ecb6bba7aa78
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-16 15:37:59 +0200

    ongoing process

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,13 +7,12 @@ class GamesController < ApplicationController
 
   def score
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
-    while LETTERS.each { |letter| params[:word].include?(letter) }
-      if url['found'] == 'false'
+    if LETTERS.each { |letter| params[:word].include?(letter) }
+      @score = "Sorry but #{params[:word].upcase} can't be built out of #{LETTERS.join.upcase}."
+    elsif url['found'] == 'false'
       @score = "Sorry but #{params[:word].upcase} doesn't seem to be an English word..."
-      elsif url['found'] == 'true'
+    elsif url['found'] == 'true'
       @score = "Congratulations! #{params[:word].upcase} is a valid English word!"
-      end
     end
-    @score = "Sorry but #{params[:word].upcase} can't be built out of #{LETTERS.join.upcase}."
   end
 end
